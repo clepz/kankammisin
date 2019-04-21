bbb660c3c19d27099bcf26b5030a276859d4a14d
clepz <tarik_41.41@hotmail.com>
2019-04-21 15:12:33 +0300

    vooooo

--- src/main/java/com/anonsgroup/kankammisin/controllers/RestController.java
+++ src/main/java/com/anonsgroup/kankammisin/controllers/RestController.java
@@ -1,5 +1,10 @@
 package com.anonsgroup.kankammisin.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
+
+
+
 }
