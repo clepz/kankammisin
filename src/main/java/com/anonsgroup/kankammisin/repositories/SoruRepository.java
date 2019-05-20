cc2dcc82895dad0b35fd168df567597632830edf
clepz <tarik_41.41@hotmail.com>
2019-05-20 13:38:07 +0300

    Merge remote-tracking branch 'remotes/origin/master'
    # Conflicts:
    #	src/main/resources/templates/testolustur.html

--- src/main/java/com/anonsgroup/kankammisin/repositories/SoruRepository.java
+++ src/main/java/com/anonsgroup/kankammisin/repositories/SoruRepository.java
@@ -3,6 +3,7 @@ package com.anonsgroup.kankammisin.repositories;
 import com.anonsgroup.kankammisin.model.Soru;
 import com.anonsgroup.kankammisin.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,5 +14,8 @@ public interface SoruRepository extends JpaRepository<Soru,Integer> {
     List<Soru> findByUser(User user);
 
     List<Soru> findAllByTest_TestId(Long id);
+
+    @Transactional
+    void deleteAllByTestTestId(Long id);
 
 }
